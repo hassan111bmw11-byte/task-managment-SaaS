bac75d0025dd4bd0dcb91294011c63fb70cff621
hassan111bmw11-byte <hassan111.bmw11@gmail.com>
2026-03-27 20:42:14 +0300

    aaaaaa

--- app/components/projectCard.js
+++ app/components/projectCard.js
@@ -46,9 +46,9 @@ export default function ProjectCard() {
       if (!data) {
         return <p>loading...</p>;
       }
-      console.log(user.data?._id);
+      console.log(user?.data?._id);
       const pro = data?.filter((p) => {
-        return p.owner === user.data?._id;
+        return p.owner === user?.data?._id;
       });
 
       console.log(pro);
@@ -65,7 +65,7 @@ export default function ProjectCard() {
     try {
       // localStorage.getItem("user");
       const user = JSON.parse(localStorage.getItem("data")) || [];
-      const ownerId = user.data?._id;
+      const ownerId = user?.data?._id;
       const res = await fetch(
         `https://demo-rrxv.onrender.com/create/${ownerId}`,
         {
